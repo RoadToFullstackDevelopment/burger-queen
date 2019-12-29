bfd641bf8c2b6f383b5f33ecfe4f7606c8c51b5c
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2019-12-29 17:55:08 -0300

    Reinstalação de alguns arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import Main from './screens/Main';
-import Register from './screens/Register';
-import Login from './screens/Login';
-import Kitchen from './screens/Kitchen';
-import Salon from './screens/Salon';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/kitchen" component={Kitchen} />
-        <Route path="/salon" component={Salon} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+    <div>
+      <h1>Teste</h1>
+    </div>
+  )
 }
 
 export default App
