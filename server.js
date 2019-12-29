b8e35e45142e1b75e256da2b15fe8c310de42fee
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2019-12-29 19:13:40 -0300

    Inclusão de servidor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = './public/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
