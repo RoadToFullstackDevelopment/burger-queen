811970687e73b74c14d165e2100721aa7987a20e
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2019-12-30 17:47:12 -0300

    Inclusão de rotas nos botões da tela principal

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <h1>Burger Queen</h1>
-            <div><button>ENTRAR</button></div>
-            <div><button>REGISTRAR</button></div>
+            <div><Link to="/login"><button>ENTRAR</button></Link></div>
+            <div><Link to="/register"><button>REGISTRAR</button></Link></div>
         </div>
     )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -25,4 +25,4 @@ const Register = () => {
     )
 }
 
-export default Register
+export default Register;
