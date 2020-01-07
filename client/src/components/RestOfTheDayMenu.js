8045764dbbf335afb2a22c27c24dd0a9ea8d7eac
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2020-01-07 18:19:55 -0300

    Inclusão de menus na parte do salão

---
+++ client/src/components/RestOfTheDayMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const RestOfTheDayMenu = () => {
+    return (
+        <main>
+            <div><h1>Salão</h1></div>
+            <div><button><Link to="/salon">VOLTAR PARA A TELA PRINCIPAL</Link></button></div>
+        </main>
+    )
+}
+export default RestOfTheDayMenu;
--- client/src/components/Salon.js
+++ client/src/components/Salon.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Auth from './Auth';
+import BreakfastMenu from './BreakfastMenu';
+import RestOfTheDayMenu from './RestOfTheDayMenu';
+import { Link } from 'react-router-dom';
 
 const Salon = (props) => {
     return (
         <div>
            <div><h1>Salão</h1></div> 
-           <div><button>CAFÉ DA MANHÃ</button></div>
-           <div><button>RESTO DO DIA</button></div>
+           <div><button><Link to="/breakfast">CAFÉ DA MANHÃ</Link></button></div>
+           <div><button><Link to="/restoftheday">RESTO DO DIA</Link></button></div>
            <div><button>PEDIDOS PRONTOS</button></div>
            <div><button onClick={() => {Auth.logout(() => {props.history.push("/")})}}>SAIR</button></div>
         </div>
